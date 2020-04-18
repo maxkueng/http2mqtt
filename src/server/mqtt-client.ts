15041975da6f3e506ce887de02783ad49dedea65
Max Kueng <me@maxkueng.com>
2020-04-18 14:43:41 +0200

    Add function to subscribe to MQTT topics

--- src/server/mqtt-client.ts
+++ src/server/mqtt-client.ts
@@ -18,10 +18,34 @@ export default class MqttClient extends EventEmitter {
 
   client: MQTT | undefined;
 
+  topicHandlers: { [topic: string]: Array<(message: string) => void> } = {};
+
   constructor(options: MqttConfiguration) {
     super();
 
     this.options = options;
+  }
+
+  handleMessage = (topic: string, payload: Buffer): void => {
+    const handlers = this.topicHandlers[topic] || [];
+    handlers.forEach((handler) => {
+      handler(payload.toString());
+    });
+  };
+
+  addTopicHandler(topic: string, handler: (message: string) => void): void {
+    if (this.topicHandlers[topic]) {
+      this.topicHandlers[topic].push(handler);
+    } else {
+      this.topicHandlers[topic] = [handler];
+    }
+  }
+
+  removeTopicHandler(topic: string, handler: (message: string) => void): void {
+    const handlers = this.topicHandlers[topic];
+    if (handlers) {
+      this.topicHandlers[topic] = handlers.filter((h) => h !== handler);
+    }
   }
 
   connect(): Promise<void> {
@@ -40,6 +64,8 @@ export default class MqttClient extends EventEmitter {
       this.client.on('connect', () => {
         resolve();
       });
+
+      this.client.on('message', this.handleMessage);
     });
   }
 
@@ -67,5 +93,16 @@ export default class MqttClient extends EventEmitter {
         resolve();
       });
     });
+  }
+
+  subscribe(topic: string, handler: (message: string) => void): () => void {
+    if (!this.client) {
+      throw new Error('Unexpected code reached');
+    }
+    this.addTopicHandler(topic, handler);
+    this.client.subscribe(topic);
+    return (): void => {
+      this.removeTopicHandler(topic, handler);
+    };
   }
 }
